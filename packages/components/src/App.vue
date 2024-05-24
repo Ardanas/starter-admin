f795046683e86f826de86ca69a5ea48d5c111a11
ardans <ardans@foxmail.com>
2024-05-24 15:41:16 +0800

    feat(components): update theme

--- packages/components/src/App.vue
+++ packages/components/src/App.vue
@@ -3,14 +3,9 @@
 
 <template>
   <div>
-    <el-button>hello</el-button>
-
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <el-button type="primary">
+      hello
+    </el-button>
   </div>
 </template>
 
--- packages/components/vite.config.js
+++ packages/components/vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -5,13 +6,28 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5176
+  },
   plugins: [
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element-plus.scss" as *;`,
+      },
+    },
+  }
 })
