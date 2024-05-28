459b138812608177b653a2bf0a3c363a5b0ed29b
ardans <ardans@foxmail.com>
2024-05-28 10:18:36 +0800

    feat: add constants

---
+++ packages/constants/src/dataType.js
@@ -0,0 +1,16 @@
+export const VALUE_MAP = {
+  STRING: 'java.lang.String',
+  INTEGER: 'java.lang.Integer',
+  BOOLEAN: 'java.lang.Boolean',
+  LONG: 'java.lang.Long',
+  DOUBLE: 'java.lang.Double',
+  SHORT: 'java.lang.Short',
+  BYTE: 'java.lang.Byte',
+  CHARACTER: 'java.lang.Character',
+  FLOAT: 'java.lang.Float',
+  MAP: 'java.util.Map',
+  LIST: 'java.util.List',
+  OBJECT: 'java.lang.Object',
+  JSONOBJECT: 'com.alibaba.fastjson.JSONObject',
+  VOID: 'VOID',
+}
